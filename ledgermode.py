f5d04078bd376c578bac1572cc8dbc4fd9284547
Rob Napier <robnapier@gmail.com>
2017-09-17 12:25:34 -0400

    Balances!

--- ledgermode.py
+++ ledgermode.py
@@ -1,4 +1,4 @@
-# import sublime
+import sublime
 import sublime_plugin
 import subprocess
 
@@ -15,32 +15,52 @@ def ledger(args, input=None):
                          universal_newlines=True)
     stdout, stderr = p.communicate(input)
 
-    return stdout
+    return stdout.strip()
 
 
 def set_ledger_output(edit, window, text):
-    panel = window.create_output_panel("ledger")
+    panel = window.create_output_panel('ledger')
     panel.set_read_only(False)
+    region = sublime.Region(0, panel.size())
+    panel.erase(edit, region)
     panel.insert(edit, 0, text)
     panel.set_read_only(True)
+    panel.show(0)
     window.run_command("show_panel", {"panel": "output.ledger"})
 
 
-class PromptBalanceCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.show_input_panel(
-            "Account:", "", self.on_done, None, None)
+class LedgerBalanceCommand(sublime_plugin.TextCommand):
 
-    def on_done(self, text):
-        try:
-            if self.window.active_view():
-                self.window.active_view().run_command(
-                    "balance", {"account": text})
-        except ValueError:
-            pass
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.selected_index = None  # type: int
+        self.accounts = None  # type: list
 
+    def run(self, edit):
+        if self.selected_index is None:
+            text = self.view.substr(sublime.Region(0, self.view.size()))
+            accounts_output = ledger(['accounts', '^Assets:', '^Liabilities:'],
+                                     input=text)
 
-class BalanceCommand(sublime_plugin.TextCommand):
-    def run(self, edit, account):
-        result = ledger(["bal", account])
-        set_ledger_output(edit, self.view.window(), result)
+            if len(accounts_output) == 0:
+                accounts_output = ledger(['accounts'])
+
+            self.accounts = accounts_output.split("\n")
+
+            self.view.window().show_quick_panel(self.accounts,
+                                                self.input_account)
+        else:
+            if self.selected_index >= 0:
+                account = self.accounts[self.selected_index]
+                self.show_balance(edit, account)
+
+            self.selected_index = None
+            self.accounts = None
+
+    def input_account(self, index):
+        self.selected_index = index
+        self.view.run_command('ledger_balance')
+
+    def show_balance(self, edit, account):
+        balance = ledger(['balance', account])
+        set_ledger_output(edit, self.view.window(), balance)
